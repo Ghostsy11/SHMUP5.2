ad3ce6a2ea3b5a387089d2d89b43ba54310a3b09
Ghostsy11 <115157824+Ghostsy11@users.noreply.github.com>
2024-09-30 14:59:37 +0200

    MainMenuPrototype

--- Assets/Statics/GameManager.cs
+++ Assets/Statics/GameManager.cs
@@ -1,18 +1,29 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class GameManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+
+    public void LoadScene()
     {
-        
+        StaticLoadSceneFunction(1);
     }
 
-    // Update is called once per frame
-    void Update()
+    public void ControlTutorial()
     {
-        
+        Debug.Log("Open control tutorial panel");
     }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+
+    private void StaticLoadSceneFunction(int indexOfScene)
+    {
+        SceneManager.LoadScene(indexOfScene);
+    }
+
 }
